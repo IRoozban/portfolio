acc0b71842c100e072d9e1e7aae28b1b8a37b508
iliaroozban <94915568+iliaroozbann@users.noreply.github.com>
2023-08-08 21:03:27 +0330

    added the loading page and also the main about(not styled yet) via axios

--- src/pages/component/About.jsx
+++ src/pages/component/About.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useAppContext } from "./context";
+import "../../axios";
+import "../style/about.css";
+
+//url
+
+const url = "https://api.github.com/users/iliro6";
 
 const About = () => {
-  return (
-    <div></div>
-  )
-}
+  const {
+    setIsLoading,
+    isLoading,
+    setContent,
+    content,
+    setAnimationOn,
+    animationOn,
+    IsDark,
+  } = useAppContext();
 
-export default About
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.get(url);
+      setContent(res.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setAnimationOn(!animationOn);
+    }, 1500);
+  }, [animationOn]);
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const { name, avatar_url, location, bio } = content;
+
+  if (isLoading) {
+    return (
+      <div className={`loading-section ${IsDark || "dark-text"}`}>
+        <div className="text-section">
+          <p className="loading-text"> Please Wait</p>
+        </div>
+        <div className="dots-container">
+          <div
+            className={`dot ${IsDark || "dot-dark"} ${
+              animationOn && "first-dot"
+            }`}
+          ></div>
+          <div
+            className={`dot  ${IsDark || "dot-dark"} ${
+              animationOn && "second-dot"
+            }`}
+          ></div>
+          <div
+            className={`dot  ${IsDark || "dot-dark"} ${
+              animationOn && "third-dot"
+            }`}
+          ></div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="main-section">
+      <img src={avatar_url} alt="ayo" />
+      <h2>{`hi my name is ${name} `}</h2>
+      <h2>I am a frontend Developer from {location}</h2>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/component/context.jsx
+++ src/pages/component/context.jsx
@@ -4,9 +4,24 @@ const GlobalContext = createContext();
 
 const AppContext = ({ children }) => {
   const [IsDark, setIsDark] = React.useState(false);
+  const [content, setContent] = React.useState({});
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [animationOn , setAnimationOn] = React.useState(true)
+
 
   return (
-    <GlobalContext.Provider value={{ IsDark, setIsDark }}>
+    <GlobalContext.Provider
+      value={{
+        IsDark,
+        setIsDark,
+        setIsLoading,
+        isLoading,
+        setContent,
+        content,
+        setAnimationOn,
+        animationOn,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
